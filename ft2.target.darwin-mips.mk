b5b03aa9eb5f82bc59bd7d3e140fc4d360b5f56d
Ben Murdoch <benm@google.com>
2013-06-28 19:56:37 +0100

    Fixup MIPS makefiles
    Change-Id: I3158b7faf18b3b39ac892ded457e77793f6b3638

--- ft2.target.darwin-mips.mk
+++ ft2.target.darwin-mips.mk
@@ -59,7 +59,6 @@ MY_CFLAGS := \
 	-Wno-format \
 	-EL \
 	-mhard-float \
-	--sysroot=/usr/local/google2/src/clank/webview/external/chromium_org/mipsel-sysroot/sysroot \
 	-ffunction-sections \
 	-funwind-tables \
 	-g \
@@ -139,8 +138,6 @@ LOCAL_LDFLAGS := \
 	-fPIC \
 	-EL \
 	-Wl,--no-keep-memory \
-	--sysroot=/usr/local/google2/src/clank/webview/external/chromium_org/mipsel-sysroot/sysroot \
-	 \
 	-nostdlib \
 	-Wl,--no-undefined \
 	-Wl,--exclude-libs=ALL \
