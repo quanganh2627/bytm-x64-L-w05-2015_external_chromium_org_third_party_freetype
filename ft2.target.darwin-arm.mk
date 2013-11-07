4edb5d8a95be012d24ec24b35d09ada250e9f936
Android Chromium Automerger <chromium-automerger@android>
2013-11-07 14:36:20 +0000

    Update makefiles after merge of Chromium at 233612
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: I9da6b26e8679f007ed51bf9ac6a57395a5ee4ac0

--- ft2.target.darwin-arm.mk
+++ ft2.target.darwin-arm.mk
@@ -82,6 +82,7 @@ MY_CFLAGS_Debug := \
 
 MY_DEFS_Debug := \
 	'-DANGLE_DX11' \
+	'-DWebKit=blink' \
 	'-D_FILE_OFFSET_BITS=64' \
 	'-DNO_TCMALLOC' \
 	'-DDISABLE_NACL' \
@@ -166,6 +167,7 @@ MY_CFLAGS_Release := \
 
 MY_DEFS_Release := \
 	'-DANGLE_DX11' \
+	'-DWebKit=blink' \
 	'-D_FILE_OFFSET_BITS=64' \
 	'-DNO_TCMALLOC' \
 	'-DDISABLE_NACL' \
