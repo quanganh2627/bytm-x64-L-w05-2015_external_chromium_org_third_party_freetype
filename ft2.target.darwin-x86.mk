357c87a5302c5d7c02c10870ebe7a78652807497
Android Chromium Automerger <chromium-automerger@android>
2014-01-17 04:23:28 +0000

    Update makefiles after merge of Chromium at 245421
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: I85657df8f86a043a80a83a37d6f2f40235ae0b62

--- ft2.target.darwin-x86.mk
+++ ft2.target.darwin-x86.mk
@@ -80,8 +80,7 @@ MY_CFLAGS_Debug := \
 	-g \
 	-fomit-frame-pointer \
 	-fdata-sections \
-	-ffunction-sections \
-	-funwind-tables
+	-ffunction-sections
 
 MY_DEFS_Debug := \
 	'-DV8_DEPRECATION_WARNINGS' \
@@ -174,7 +173,8 @@ MY_CFLAGS_Release := \
 	-fdata-sections \
 	-ffunction-sections \
 	-fomit-frame-pointer \
-	-funwind-tables
+	-fno-unwind-tables \
+	-fno-asynchronous-unwind-tables
 
 MY_DEFS_Release := \
 	'-DV8_DEPRECATION_WARNINGS' \
