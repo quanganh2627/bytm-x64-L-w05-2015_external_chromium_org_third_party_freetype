b2a4be12f8f7ef9305e20e0d3b19c3fce4441f3c
Android Chromium Automerger <chromium-automerger@android>
2014-03-12 00:41:29 +0000

    Update makefiles after merge of Chromium at 256368
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: Iefd4afa427efbe6ff509435510f716a1c76797c9

--- ft2.target.darwin-x86.mk
+++ ft2.target.darwin-x86.mk
@@ -55,11 +55,10 @@ MY_CFLAGS_Debug := \
 	-pipe \
 	-fPIC \
 	-Wno-format \
-	-m32 \
-	-mmmx \
-	-march=pentium4 \
 	-msse2 \
 	-mfpmath=sse \
+	-mmmx \
+	-m32 \
 	-fuse-ld=gold \
 	-ffunction-sections \
 	-funwind-tables \
@@ -151,11 +150,10 @@ MY_CFLAGS_Release := \
 	-pipe \
 	-fPIC \
 	-Wno-format \
-	-m32 \
-	-mmmx \
-	-march=pentium4 \
 	-msse2 \
 	-mfpmath=sse \
+	-mmmx \
+	-m32 \
 	-fuse-ld=gold \
 	-ffunction-sections \
 	-funwind-tables \
